3aea16808061e84e3047fb5252a6472dc9d0956c
Stan <kohler.stanislas@gmail.com>
2019-06-07 13:04:28 +0200

    Concomitance V2 ok

--- src/AlgoTestFormulesSandBox.py
+++ src/AlgoTestFormulesSandBox.py
@@ -24,12 +24,21 @@ Travaillons sur un algorithme pour travailler la formule
 
 #  Liste des formules à étudier
 
-f1 = "Zeus / Hêlios / Megas / Sarapis / Zeus / Zeus"
+f1 = "Zeus / Hêlios / Megas / Sarapis"
 f2 = "[Apollôn # Puthios] + [Apollôn # Kedrieus]"
 f3 = "Apollôn # (Puthios + Kedrieus)"
-f4 = "([Kurios # Zeus] + Hêra ) # Epêkoos"
+f4 = "([Kurios # Zeus] + Hêra) # Epêkoos"
 f5 = "[Ammôn = Chnoubis] + [Hêra = Satis] + [Hestia = Anoukis]"
 f6 = "(Zeus + Hêra) # Sôtêr"
+f7 = "[Theos # Sôtêr] # (Artemis + Apollôn)"
+f8 = "[Zeus # Sôtêr] + (Athêna # Sôtêr)"
+f9 = "[Zeus # Boulaios] + [Athêna # Boulaios] + Hestia"
+f10 = "Artemis # Puthios"
+f11 = "Dionusos # Phleos"
+f12 = "[Zeus # Brontôn] + [Zeus # Karpodotês] + [Zeus # Eucharistos]"
+
+# Liste des attestations
+attestations = [f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12]
 
 """Liste des Dieux et Mots"""
 
@@ -49,7 +58,17 @@ noms = {
     14: "Anoukis",
     15: "Sôtêr",
     16: "Kurios",
-    17: "Hestia"
+    17: "Hestia",
+    18: "Artemis",
+    19: "Theos",
+    20: "Athêna",
+    21: "Puthios",
+    22: "Dionusos",
+    23: "Phloes",
+    24: "Brontôn",
+    25: "Karpodotês",
+    26: "Eucharistos"
+
 }
 """ Liste des signes pour les formules """
 
@@ -64,27 +83,14 @@ signes = {
     8: "=",
     9: " "
 }
-# résultat pour F1
-"""Si split aux / Je garde que les noms"""
-# res = f1.split("/")
+
+""" Foctionne avec variable"""
 
 
-"""Affichage de res après boucle"""
-# res = f1.split(" ")
-# for i in res:
-#     print(i)
-
-"""Affichage avec le dictionnaire"""
-# for key, valeur in dico.items():
-#     print(valeur)
-
-"""Combinaison V1 OK"""
-
-
-def coocurence(formule):  # Permet de calculer la Coocurence des formules
+def coocurenceVar(formule):  # Permet de calculer la Coocurence des formules
     """
     :param formule
-    :return Le nombre de fois qu'un nom appraît dans la formule
+    :return Le nombre de fois qu'un nom appraît dans la formule et les liens avec les autres noms
 
     """
 
@@ -97,12 +103,14 @@ def coocurence(formule):  # Permet de calculer la Coocurence des formules
     temp = []  # Liste temporaire
 
     for i in res:  # Permet de parcourir les élément de la liste res
-        temp.append(" ")  # Ajoute chaque
+        temp.append(" ")  # Ajoute chaque element à la liste coupé a l'espace
         for j in i:
             if j not in signes.values():  # Ajoute les caractères non spéciaux
                 temp.append(j)
 
     del temp[0]  # Supprime l'espace ajouté au début de la liste
+
+    """Calcule la taille des noeuds"""
     chaine = ""  # Création de la chaine vide qui sera split par l'espace
 
     for i in temp:  # Parcours la liste temp pour la traduire en chaine de caractère
@@ -114,6 +122,69 @@ def coocurence(formule):  # Permet de calculer la Coocurence des formules
         compte[valeur] += 1
     return compte  # Renvoie la liste
 
+    """Calcule les taille des arcs"""
+
+
+"""Cooccurence avec liste en entrée"""
+
+
+def coocurenceListe(liste):  # Permet de calculer la Coocurence des formules
+    """
+    :param Liste de formules
+    :return Le nombre de fois qu'un nom appraît dans la formule
+
+    """
+    noeud = {}  # Permet de stocker la taille des noeuds
+    cle = 0  # Clé pour le dictionnaire à incrémenter
+    noeud2 = {}  # Dictionnaire avec les nombre de noms mentionnés dans les formules
+
+    # Lit pour chaque élément de la liste
+    for element in liste:  # Applique pour chaque élément de la liste le traitement de la formule
+        formule = element
+
+        res = formule.split(" ")  # Permet de découper la formule en supprimant les espaces
+        for i in res:
+            for j in signes.values():  # Regarde dans le dictionnaire des signes si il apparait
+                if i == j:
+                    res.remove(j)  # Supprime les signes spéciaux des formules
+
+        temp = []  # Liste temporaire
+
+        # A mettre dans une autre fonction
+        for i in res:  # Permet de parcourir les élément de la liste res
+            temp.append(" ")  # Ajoute chaque element à la liste coupé a l'espace
+            for j in i:
+                if j not in signes.values():  # Ajoute les caractères non spéciaux
+                    temp.append(j)
+
+        del temp[0]  # Supprime l'espace ajouté au début de la liste
+
+        # A mettre dans une autre fonction
+        """Calcule la taille des noeuds par formule"""
+        chaine = ""  # Création de la chaine vide qui sera split par l'espace
+        for i in temp:  # Parcours la liste temp pour la traduire en chaine de caractère
+            chaine += i
+        final = chaine.split(" ")  # Coupe la chaine de caractère à l'espace pour en refaire uen liste
+        compte = {}.fromkeys(set(final), 0)  # Définit le compteur pour chaque mot présent
+
+        for valeur in final:  # Compte le nombre d'occurence de mots dans la liste
+            compte[valeur] += 1
+        noeud[cle] = compte  # Ajoute le nombre de fois qu'apparait un nom dans chaque formule
+        cle += 1  # Incrémente la clé du dictionnaire de stockage
+
+        """Calcule le nombre de fois qu'un nom apparait dans toutes les formules"""
+        #  A mettre dans une autre fonction
+
+    for element in noeud.values():  # Parcous le dictionnaire de dictionnaire
+        for i in element:  # Parcours chaque élémément du dictionnaire
+            if i not in noeud2.keys():  # Regarde si un élement est dans la liste sinon l'ajoute
+                noeud2[i] = 1  # Ajoute l'élément et l'initialise a 1
+            else:
+                noeud2[i] += 1  # Incrémente la valeur si celui ci est présent
+
+    return noeud2  # Revoie le dictionnaire avec les nobres d'apparition des noms
+
 
 if __name__ == "__main__":
-    print(coocurence(f1))
+    # print(coocurenceVar(f1))
+    print(coocurenceListe(attestations))
